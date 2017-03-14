2d0e560f8f5ffe37d0e4ad6c6f4a43dea51b5169
MariaGomezPerez <beni-13@hotmail.com>
2017-03-13 18:22:33 -0600

    1

---
+++ index2.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    <title>Practica 10 php </title>
+    </head>
+<body>
+    <h1>Usando PHP</h1> 
+    <p><?php
+    echo "Parrafo php";
+    ?></p>
+     <p>Parrafo HTML</p>
+    <h3>
+    <?php
+        echo "Parrafo php";
+    ?>
+ 
+    </h3>
+
+
+</body>
+</html>
